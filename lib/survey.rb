5fb8847d2c27cf399154070d0e6f1c76c7072f03
Mitch Long and Megan Olson <mitch@>
2017-09-26 13:53:47 -0700

    add callback method to capitalize first letter of e=survey input

--- lib/survey.rb
+++ lib/survey.rb
@@ -1,3 +1,11 @@
 class Survey < ActiveRecord::Base
   has_many(:questions)
+  before_save(:capitalize_first_letter)
+
+
+private
+  def capitalize_first_letter
+    title = self.title.split(" ")
+    self.title = title.map{|word| word.capitalize}.join(" ")
+  end
 end
